38b70cddb172fc7cd97dc19a19ab983a01ac374f
Yitzkak <bevinchi@gmail.com>
2020-06-29 19:18:40 +0100

    The homepage is completed here

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// Make it responsive
+// Add another space at the bottom and make it down when clicked
+// Complete footer
+// Make the image responsible
+// Add animation to the button
+// Complete this project
+
+
+
+// What bem video
+// what sass video
